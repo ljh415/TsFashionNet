45c7ac454588ebe01d8e907b524efb43e262168c
ljh415 <ljh415@gmail.com>
2022-09-24 00:55:48 +0900

    add transform, some comments

--- dataset.py
+++ dataset.py
@@ -5,10 +5,14 @@ from PIL import Image
 
 import torch
 import torch.nn.functional as F
-import torchvision.transforms as transforms
 from torch.utils.data import Dataset
 
 from utils import lm_transforms
+
+# 이거 데이터셋을 굳이 두가지로 만들이유가 있을까?
+# 하나의 데이터셋으로 한다음에 그냥 사용만 안하고 필요한것만 넣어서 쓰면 되는거잖아..?
+# 어차피 에폭단위로 배치사이즈는 다시 도니까...
+
 
 LANDMARK_MAP = ['left_collar', 'right_collar', 'left_sleeve', 'right_sleeve',
                 'left_waistline', 'right_waistline', 'left_hem', 'right_hem']
@@ -104,8 +108,10 @@ class ShapeDataset(Dataset):
         return img, landmark, visibility
 
 class TSDataset(Dataset):
-    def __init__(self, data_path):
+    def __init__(self, data_path, transform=None):
         super(TSDataset, self).__init__()
+        
+        self.transform = transform
         with open(data_path, 'rb') as f:
             raw_dict = pickle.load(f)
         self.raw_data = list(raw_dict.items())
@@ -120,7 +126,7 @@ class TSDataset(Dataset):
                 visibility.append(1 if landmark_info[lm][0] == 0 else 0)
             else :
                 visibility.append(0)
-        return visibility
+        return torch.Tensor(visibility)
 
     def _make_landmark(self, img_size, landmark_info):
         height, width = img_size
@@ -174,21 +180,29 @@ class TSDataset(Dataset):
                 target[joint_id][img_y[0] : img_y[1], img_x[0]:img_x[1]] = g[g_y[0]:g_y[1], g_x[0]:g_x[1]]
         return target, visibility
 
-    def __getitem__(self, index, crop):
+    def __getitem__(self, index, crop=True):
         img_path, data_dict = self.raw_data[index]
         landmark_info = data_dict['landmark']
         
         img_path = os.path.join(IMG_DIR, img_path)
+        if "Striped_A-Line_Dress" in img_path:
+            img_path = img_path.replace("A-Line", "A-line")
         img = Image.open(img_path).convert("RGB")
-        landmark, visibility = self._make_landmark(img.size, landmark_info)
         
-        category = F.one_hot(torch.LongTensor(data_dict['category']), 46)
+        landmark, visibility = self._make_landmark(img.size, landmark_info)
+        category = torch.LongTensor(data_dict['category'])
+        
+        # 논문에서는 46개라고 했는데 확인해본 결과 총 48개의 라벨이 존재
         attribute = torch.Tensor([1 if x == 1 else 0 for x in data_dict['attr']])
         
         if crop:
             bbox = data_dict['bbox']
-            img = np.array(img)[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+            img = img.crop(bbox)
+            # img = Image.fromarray(np.array(img)[bbox[1]:bbox[3], bbox[0]:bbox[2]])
             landmark = landmark[..., bbox[1]:bbox[3], bbox[0]:bbox[2]]
-            landmark = torch.transpose(landmark, (1, 2, 0))
+        
+        if self.transform:
+            img = self.transform(img)
+            landmark = lm_transforms(self.transform, landmark)
         
         return img, category, attribute, visibility, landmark
