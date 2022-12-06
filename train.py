0c55352ab3ea2ffd59c4f5af5c4089ed0a4be4ff
ljh415 <ljh415@gmail.com>
2022-12-06 22:20:08 +0900

    change num_workers 15 to 16

--- train.py
+++ train.py
@@ -486,7 +486,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_path", type=str, default='/media/jaeho/SSD/datasets/deepfashion/split/')
     parser.add_argument("--epochs", type=int, default=12)
     parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--num_workers", type=int, default=15)
+    parser.add_argument("--num_workers", type=int, default=32)
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--resolution", type=int, default=224)
     parser.add_argument("--project", type=str, default="TSFashionNet")
