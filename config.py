8601f963b9d11aade4ad78dec556ab93747f4449
ljh415 <ljh415@gmail.com>
2023-01-15 15:33:01 +0900

    mod sweep config, remove batch_size=64, max lr 1e-2

--- config.py
+++ config.py
@@ -24,10 +24,10 @@ sweep_configuration = {
         'name': 'valid_loss'
     },
     'parameters': {
-        'batch_size': {'values':[16, 32, 64]},
+        'batch_size': {'values':[16, 32]},
         'epochs': {'min':12, 'max':20},
-        'lr': {'min':1e-5, 'max':1e-3},
+        'lr': {'min':1e-5, 'max':1e-2},
         'shape_epochs': {'min': 3, 'max': 5},
-        'shape_lr': {'min':1e-4, 'max':1e-3},
+        'shape_lr': {'min':1e-4, 'max':1e-2},
     }
 }
