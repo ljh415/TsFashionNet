0c99a8b24f17bbe82fbccbbfa7e6edde92fb71bb
ljh415 <ljh415@gmail.com>
2023-01-28 18:20:58 +0900

    mod vgg sweep config

--- config.py
+++ config.py
@@ -25,11 +25,11 @@ sweep_configuration = {
             'name': 'valid_loss'
         },
         'parameters': {
-            'batch_size': {'values':[16, 32]},
+            # 'batch_size': {'values':[16, 32]},
             'epochs': {'min':12, 'max':20},
             'lr': {'min':1e-5, 'max':1e-2},
-            'shape_epochs': {'min': 3, 'max': 5},
-            'shape_lr': {'min':1e-5, 'max':5e-3},
+            # 'shape_epochs': {'min': 3, 'max': 5},
+            # 'shape_lr': {'min':1e-5, 'max':5e-3},
         }
     },
     'bit': {
