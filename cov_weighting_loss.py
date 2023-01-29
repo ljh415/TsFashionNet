f6601a21e7e6246f039d80a94b49431f33607bef
ljh415 <ljh415@gmail.com>
2023-01-29 21:31:14 +0900

    first commit of cov_weighting

---
+++ cov_weighting_loss.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CoVLoss(nn.Module):
+    def __init__(self):
+        pass
+    
+    def forward(self, pred, target):
+        pass
