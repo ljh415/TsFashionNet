d0e7f0469eafaa79f8861c01be501324f2466f89
ljh415 <ljh415@gmail.com>
2023-01-30 18:06:52 +0900

    mod max lr of vgg sweep config

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ sweep_configuration = {
         'parameters': {
             # 'batch_size': {'values':[16, 32]},
             'epochs': {'min':12, 'max':20},
-            'lr': {'min':1e-5, 'max':1e-2},
+            'lr': {'min':1e-5, 'max':3e-3},
             # 'shape_epochs': {'min': 3, 'max': 5},
             # 'shape_lr': {'min':1e-5, 'max':5e-3},
         }
